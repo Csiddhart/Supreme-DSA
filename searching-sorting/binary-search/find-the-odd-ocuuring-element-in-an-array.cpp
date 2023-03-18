ca8d287feb9be75e6a02a3e50c59e05e320882b5
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-19 02:41:31 +0530

    Create find-the-odd-ocuuring-element-in-an-array.cpp

---
+++ searching-sorting/binary-search/find-the-odd-ocuuring-element-in-an-array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solve(vector<int> arr){
+  int s=0;
+  int e=arr.size()-1;
+  int mid= s+(e-s)/2;
+  while(s<=e){
+    if(s==e){
+      //single element
+      return s;
+    }
+
+    //here two cases are there -> mid is even  or  mid is odd
+    if(mid%2==0){
+      if(arr[mid]==arr[mid+1]){
+        s=mid+2;
+      }else{
+        e=mid;
+      }
+    }else{
+      if(arr[mid]==arr[mid-1]){
+        s=mid+1;
+      }else{
+        e= mid-1;
+      }
+    }
+    mid=s+(e-s)/2;
+  }
+  return -1;
+}
+
+int main() {
+  vector<int>arr{1,1,2,2,3,3,4,4,2,6,6,4,4};
+  int ans= solve(arr);
+  cout<<"ans is index  "<<ans<<endl;
+  return 0;
+}
