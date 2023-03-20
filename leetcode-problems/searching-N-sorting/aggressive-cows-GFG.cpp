acd9f58cb72b789b8e22d75f338350941ccbe63b
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-20 23:56:48 +0530

    Create aggressive-cows-GFG.cpp

---
+++ leetcode-problems/searching-N-sorting/aggressive-cows-GFG.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+  
+  
+bool ispossible(vector<int> &stalls,int k, int mid){
+    //can we place k cows, with at least mid distances b/w cows 
+    int c=1;
+    int pos =stalls[0];
+    
+    for(int i=0;i<stalls.size();i++){
+        if(stalls[i]- pos >=mid){
+            c++;
+            pos = stalls[i];  //one more cow has been placed 
+        }
+        if(c==k){
+            return true;
+        }
+    }
+    return false;
+}
+  
+  
+    int solve(int n, int k, vector<int> &stalls) {
+     sort(stalls.begin(),stalls.end());
+     int start=0;
+     int end= stalls[stalls.size()-1]-stalls[0];
+     
+     int ans =-1;
+     
+     while(start<=end){
+         int mid= (start+end)>>1;
+         if (ispossible(stalls,k,mid)){
+             ans=mid;
+             start=mid+1;
+         }
+         else{
+             end=mid-1;
+         }
+     }
+     return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
