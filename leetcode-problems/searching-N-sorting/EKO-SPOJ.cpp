ab69e20bf4e1584cb1f0cf23a11fdc6afc1f419a
SIDDHARTH CHATTERJEE <108140221+Csiddhart@users.noreply.github.com>
2023-03-21 10:49:23 +0530

    Create EKO-SPOJ.cpp

---
+++ leetcode-problems/searching-N-sorting/EKO-SPOJ.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool ispossible(vector<long long int > trees,long long int m,long long int mid){
+  long long int woodcollected =0;
+  for(long long int i=0;i<trees.size();i++){
+    if(trees[i]>mid){
+      woodcollected+=trees[i]-mid;
+    }
+  }
+  return woodcollected>=m;
+}
+
+long long int maxswabladeheight(vector<long long int > trees,long long int m){
+  long long int start =0,end,ans=-1;
+  end= *max_element(trees.begin(),trees.end());
+  while(start<=end){
+    long long int mid= start+(end-start)/2;
+    if(ispossible(trees,m,mid)){
+      ans=mid;
+      start=mid+1;
+    }else{
+      end=mid-1;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  long long int n,m;
+  cin>>n>>m;
+  vector<long long int> trees;
+  while(n--){
+    long long int height ;
+    cin>>height;
+    trees.push_back(height);
+  }
+  cout<<maxswabladeheight(trees,m)<<endl;
+  return 0;
+}
